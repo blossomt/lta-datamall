009814702755dbfc1bd4f5919caf5be97a11253e
blossomt <blossomtang@me.com>
2021-09-04 04:56:35 +0800

    add .py file

---
+++ get_bus_tables.py
@@ -0,0 +1,50 @@
+import requests, csv, os
+from dotenv import load_dotenv
+from datetime import date 
+
+load_dotenv()
+key = os.environ.get('AccountKey')
+method = 'GET'
+headers = { 'AccountKey' : key,
+            'accept' : 'application/json'}
+
+#Get all data when API limits to 500 rows
+def get_all_data(base_uri,output_file):
+    end = False
+    n = 0
+    result_arr = []
+    while end == False:
+        uri = base_uri+"?$skip="+str(n)
+        response = requests.get(uri, headers=headers)
+        if response.status_code == 200:
+#             print("Getting rows "+str(n)+"+...")
+            jsonObj = response.json()
+            nrow = len(jsonObj['value'])
+            result_arr.extend(jsonObj['value'])
+            if nrow < 500:
+                end = True
+            n += nrow
+        else:
+#             print("Failed to request with error "+str(response.status_code))
+            end = True
+    #Write list
+    with open(output_file,'w',encoding='utf-8') as f:
+        title = [*result_arr[0].keys()]
+        cw = csv.DictWriter(f,title,delimiter=",")
+        cw.writeheader()
+        cw.writerows(result_arr)
+    print("Total rows: "+str(n))
+
+today = date.today().strftime("%Y%m%d")
+
+#Bus service
+bus_svc_uri = "http://datamall2.mytransport.sg/ltaodataservice/BusServices"
+#Bus route
+bus_route_uri = "http://datamall2.mytransport.sg/ltaodataservice/BusRoutes"
+#Bus stop coordinates
+bus_stop_uri = "http://datamall2.mytransport.sg/ltaodataservice/BusStops"
+
+print(today)
+get_all_data(bus_stop_uri,os.path.join('output',today+'bus_stop.csv'))
+get_all_data(bus_svc_uri,os.path.join('output',today+'bus_service.csv'))
+get_all_data(bus_route_uri,os.path.join('output',today+'bus_route.csv'))
